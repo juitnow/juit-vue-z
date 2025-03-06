d077a0b04875a8b6a5bc1d6debf6856b1eae8680
Pier Fumagalli <pier@usrz.com>
2025-03-07 00:56:21 +0100

    No `vite-plugin-dts` (fails)

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,8 @@
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
-import dts from 'vite-plugin-dts'
 
 export default defineConfig({
-  plugins: [ vue(), dts({
-    tsconfigPath: './tsconfig.app.json',
-    // root: './lib',
-    // entryRoot: './lib/index.ts',
-    rollupTypes: true,
-  }) ],
+  plugins: [ vue() ],
 
   build: {
     minify: true,
