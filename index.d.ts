1df5a09c9b16f182ab033c4d5f10c1d3956b8b35
Pier Fumagalli <pier@usrz.com>
2025-03-11 12:17:17 +0100

    Explain our hacks

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,11 @@
-import './globals'
+/* ========================================================================== *
+ * HACK WARNING                                                               *
+ * -------------------------------------------------------------------------- *
+ * Microsoft's "Api Extractor" will happily remove all "declare module 'vue'" *
+ * when bundling declaration files. So, we split this in two: we import (and  *
+ * re-export untouched) our "globals.d.ts" file that auguments the Vue module *
+ * and then simply export the definition bundled by "Api Extractor".          *
+ * ========================================================================== */
+import './lib/globals'
 export * from './dist/index'
-export * from './globals'
+export * from './lib/globals'
