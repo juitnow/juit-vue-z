881e1b3978edbdd8b6152403ab16787b890485db
Pier Fumagalli <pier@usrz.com>
2025-03-11 17:47:13 +0100

    Fix styles import order

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { i18n } from '@juit/vue-i18n'
 import { Notify } from 'quasar'
 import { createApp } from 'vue'
 
+// This must come before the "JuitWidgets" import
+import '../lib/index.scss'
+// All remaining imports
 import '../lib/globals.d.ts'
 import { JuitWidgets } from '../lib/index'
-import '../lib/index.scss'
 import App from './app.vue'
 
 // Create app
