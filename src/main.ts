bfafe5ba9154581b58e080527173eb7e8a7e6c8f
Pier Fumagalli <pier@usrz.com>
2025-03-11 13:03:23 +0100

    Fix imports

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { i18n } from '@juit/vue-i18n'
+import { Notify } from 'quasar'
 import { createApp } from 'vue'
 
+import '../lib/globals.d.ts'
 import { JuitWidgets } from '../lib/index'
 import '../lib/index.scss'
 import App from './app.vue'
@@ -8,5 +10,5 @@ import App from './app.vue'
 // Create app
 const myApp = createApp(App)
 myApp.use(i18n, { defaultLanguage: 'en-US' })
-myApp.use(JuitWidgets)
+myApp.use(JuitWidgets, { plugins: { Notify } })
 myApp.mount('#app')
